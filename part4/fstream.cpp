cce078ccb0651535b45a26ca7e5f2e21d8689f50
daheige <zhuwei313@hotmail.com>
2020-11-23 23:33:02 +0800

    func default param

---
+++ part4/fstream.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ifstream in_stream;
+    ofstream out_stream;
+
+    string in_name = "test.txt";
+    string out_name = "out.txt";
+    in_stream.open(in_name);
+    if (in_stream.fail())
+    {
+        cout << "open file " << in_name << " fail";
+        exit(1);
+    }
+
+    // 以追加的方式添加内容到文件中
+    out_stream.open("out.txt", ios::app);
+    if (out_stream.fail())
+    {
+        cout << "write file fail";
+        exit(1);
+    }
+
+    string a, b, c, d, e;
+    in_stream >> a >> b >> c >> d >> e;
+
+    out_stream << "read content \n"
+               << a << b << c << d << e << endl;
+
+    in_stream.close();
+    out_stream.close();
+
+    return 0;
+}
